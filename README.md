8f93bee8b0098d67c070b6eca8f364d1baf34d24
Pradeepa1812 <pradeepadhanam50@gmail.com>
2023-03-22 11:25:01 +0530

    setup script added

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ##  END to END Machine Learning Project
+<pre>
 1)Setting up github Repository
 
+Upgrade pip - pip install --upgrade pip
 Create New Environment - 
       conda create -p venv python==3.8 -y 
       conda activate venv/
@@ -15,3 +17,11 @@ Sync github with vscode
       git push -u origin main
       git pull
 
+.gitignore created via github create new file with python template 
+
+creating setup.py and requirements.txt file
+setup.py - Responsible for creating ML application as a package
+           we can install this package (even deploy in pypi) in our projects and can able to use
+           Building our application as a package itself
+
+creating  src/ __init__.py
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup,find_packages
+from typing import List
+
+HYPEN_DOT = "-e ."
+def get_requirements(filename: str)->List[str]:
+    requirements = []
+
+    with open(filename) as file_obj:
+        requirements = file_obj.readlines()
+        print('requirements*******',requirements)
+        requirements = [req.replace("\n"," ") for req in requirements]
+        print('after requirements*******',requirements)
+        if HYPEN_DOT in requirements:
+            requirements.remove(HYPEN_DOT)
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='pradeepa',
+    author_email='pradeepadhanam50@gmail.com',
+    packages=find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
