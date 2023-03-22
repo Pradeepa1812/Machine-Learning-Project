70bf7496af7f1b182799cae0959a6ed78e9df216
Pradeepa1812 <pradeepadhanam50@gmail.com>
2023-03-22 16:09:39 +0530

    logging and exceptions

--- src/logger.py
+++ src/logger.py
@@ -20,11 +20,7 @@ logging.basicConfig(
     format="[ %(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
 )
 
-if __name__ == "__main__":
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.info('Division by zero Error')
-        print('Exception:::::::',e,sys)
-        raise CustomException(e,sys)
+# if __name__ == "__main__":
+#         logging.info('Logging has started')
+        
    
